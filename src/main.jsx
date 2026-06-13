0903437e504ca6fc4cf1c22dfaed2e6a2e67746c
Sebastian <sebastianfenner85@gmail.com>
2026-06-13 11:31:36 +0200

    [UX_SCAN_01] Load direct UX stylesheet

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import PasswordGate from "./components/PasswordGate.jsx";
+import "./pages/UXScan01.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
